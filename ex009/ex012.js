dfb91bd34f552433b4bba301dd3ba21230e88f2a
Lucas Fagundes <49763895+lucasfagundesf@users.noreply.github.com>
2022-10-07 10:59:13 -0300

    Exercicios de condições

---
+++ ex009/ex012.js
@@ -0,0 +1,10 @@
+var agora = new Date()
+var hr = agora.getHours()
+console.log(`Agora são exatamente ${hr} horas.`)
+if(hr < 12){
+    console.log('Bom dia!')
+}else if(hora <=18){
+    console.log('Boa Tarde!')
+} else{
+    console.log('Boa noite!')
+}
