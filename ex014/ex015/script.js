2f53f0bf4a817b2ae667308d614db546a6d5762d
Lucas Fagundes <49763895+lucasfagundesf@users.noreply.github.com>
2022-10-11 09:15:09 -0300

    Exercicios de JS

---
+++ ex014/ex015/script.js
@@ -0,0 +1,30 @@
+function contar(){
+    let ini = window.document.querySelector('#inicio')
+    let fim = window.document.querySelector('#fim')
+    let passo = window.document.querySelector('#passo')
+    let res = window.document.querySelector('#res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerText = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if ( i < f){
+            for(let c = i;c <= f; c +=p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+            
+        } else {
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
