d70eedb3fecff91bd0e07d8cd637a02ee8f108d1
Lucas Fagundes <49763895+lucasfagundesf@users.noreply.github.com>
2022-10-17 19:26:54 -0300

    exercicio JS

--- ex018/script.js
+++ ex018/script.js
@@ -1,3 +1,60 @@
+let n = window.document.querySelector('#txtn')
+let sel = window.document.querySelector('#sel')
+let res = window.document.querySelector('#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >=1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
+    }
+}
+
+function naLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
 function adicionar(){
-    let n = window.document.querySelector('#txtn')
+    if(isNumero(n.value) && !naLista(n.value, valores)){
+        valores.push(Number(n.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${n.value} adicionado.`
+        sel.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já está na lista.')
+    }
+    n.value = ''
+    n.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores para finalizar!')
+    } else {
+        let total=valores.length
+        valores.sort()
+        let soma = 0
+        let media = 0
+        let menor = valores[0]
+        let maior = valores.slice(-1)
+
+        for(let pos in valores){
+            soma += valores[pos]
+        }
+        media = soma /total
+
+
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${total} números cadastrados. </p>`
+        res.innerHTML  += `<p> O menor número é ${menor}.</p>`
+        res.innerHTML += `<p> O maior número é ${maior}. </p>`
+        res.innerHTML += `<p> Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${media}.</p> `
+    }
 }
